0b9864af5e41c011ca547e07781fb90c048d54a3
Aashish Tyagi <aashish1aug@gmail.com>
2026-01-27 21:55:20 +0530

    initial Xcode project, folder structure, and module wise READMEs files

---
+++ BookExplorer/BookExplorerApp/BookExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookExplorerApp.swift
+//  BookExplorer
+//
+//  Created by Aashish Tyagi on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct BookExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookExplorer/BookExplorerTests/BookExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookExplorerTests.swift
+//  BookExplorerTests
+//
+//  Created by Aashish Tyagi on 1/27/26.
+//
+
+import Testing
+@testable import BookExplorer
+
+struct BookExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
